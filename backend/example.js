1868491a9fa3bc90395febcd27d517138fad5c0b
Robert Vinluan <robert.vinluan@gmail.com>
2012-04-18 12:31:08 -0400

    added backend

---
+++ backend/example.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var
+  util = require('util'),
+  couchdb = require('felix-couchdb'),
+  client = couchdb.createClient(5986, 'localhost'),
+  db = client.db('my-db');
+  
+	db.saveDoc('my-doc', {awesome: 'couch fun'}, function(er, ok) {
+    	if (er) throw new Error(JSON.stringify(er));
+    	util.puts('Saved my first doc to the couch!');
+  	});
+
+	db.getDoc('my-doc', function(er, doc) {
+    	if (er) throw new Error(JSON.stringify(er));
+    	util.puts('Fetched my new doc from couch:');
+    	util.p(doc);
+  	});
+
+
+http.createServer(function (req, res) {
+	var responseEnd = 'request succeeded';
+	res.writeHead(200, {'Content-Type': 'text/plain', "access-control-allow-origin": "*"});
+	res.end(responseEnd);
+
+}).listen(1337, '127.0.0.1');
+    
+console.log('Server running at http://127.0.0.1:1337/');
